adfc103b6bfa9b7848192e005b49cea9f874942f
supernovaw <ruszrj@gmail.com>
2020-01-25 23:20:17 +1000

    Views variables in MainActivity, Clear button, TextView result

--- app/src/main/java/com/example/homework121/MainActivity.java
+++ app/src/main/java/com/example/homework121/MainActivity.java
@@ -3,26 +3,43 @@ package com.example.homework121;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 	private static HashMap<String, String> subscriptions = new HashMap<>();
 
+	private EditText emailInput, nameInput;
+	private Button subscribeButton, clearButton;
+	private TextView resultTextView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initViews();
 
-		findViewById(R.id.subscribeButton).setOnClickListener(v -> {
-			String mail = String.valueOf(((EditText) findViewById(R.id.emailInput)).getText());
-			String name = String.valueOf(((EditText) findViewById(R.id.nameInput)).getText());
+		subscribeButton.setOnClickListener(v -> {
+			String mail = String.valueOf(emailInput.getText());
+			String name = String.valueOf(nameInput.getText());
 			subscribe(mail, name);
 		});
+		clearButton.setOnClickListener(v -> {
+			emailInput.setText(null);
+			nameInput.setText(null);
+			resultTextView.setText(null);
+		});
+	}
+
+	private void initViews() {
+		emailInput = findViewById(R.id.emailInput);
+		nameInput = findViewById(R.id.nameInput);
+		subscribeButton = findViewById(R.id.subscribeButton);
+		clearButton = findViewById(R.id.clearButton);
+		resultTextView = findViewById(R.id.resultTextView);
 	}
 
 	private void subscribe(String mail, String name) {
@@ -38,9 +55,7 @@ public class MainActivity extends AppCompatActivity {
 		else {
 			subscriptions.put(mail, name);
 			output = String.format(getString(R.string.subscribed_successfully), mail);
-			((EditText) findViewById(R.id.emailInput)).setText(null);
-			((EditText) findViewById(R.id.nameInput)).setText(null);
 		}
-		Toast.makeText(this, output, Toast.LENGTH_LONG).show();
+		resultTextView.setText(output);
 	}
 }
